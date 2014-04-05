74f7163d970ce0eb44a5f905320358935eea8ba0
DarkAngel <darkangel39@abv.bg>
2014-04-05 14:40:59 +0300

    Added basic instance data for ICC and another small script for Deadmines (The rest of the npc's have nice ai scripts and there is no need for LUA there).

---
+++ Northrend/Raids/Icecrown Citadel/INSTANCE_ICC.lua
@@ -0,0 +1,197 @@
+local INSTANCE_ICC = {}
+local object_link = {
+{36612,201910},
+{36612,201911},
+{36612,201563},
+{36612,202245},
+{36855,202243},
+{36855,202244},
+{37813,201825},
+{37813,202235},
+{36626,201613}, -- festergut
+{36627,201614}, -- rotface
+{-1,201612}, -- prof colision
+{-1,201372}, -- prof door
+{36678,202182},
+{36678,201376},
+{37970,201378},
+{37970,201377},
+{37955,201755},
+{37955,202183},
+{37955,201375},
+{36789,201382},
+{36789,201381},
+{36789,201380},
+{36789,201383},
+{36789,201374},
+{36789,201373},
+{36853,201369},
+{36853,202246},
+{36853,201379},
+{36853,202181},
+{-2,202223}
+}
+local scourge_ports = {{202246},{202245},{202244},{202243},{202242},{202235},{202223}}
+local tele_coords = {{1,70781},{2,70856},{3,70857},{4,70858},{5,70859},{6,70861},{7,70860}}
+local SPELL_CHILL = 69127
+local SPELL_FROZEN_THRONE_T = 70860
+
+function DoorOnLoad(iid, pPlayer)
+local id = pPlayer:GetInstanceID()
+if(INSTANCE_ICC[id] == nil)then
+	local string_data = {}
+	INSTANCE_ICC[id] = {}
+	INSTANCE_ICC[id].builddata = true
+	local result = CharDBQuery("SELECT killed_npc_guids FROM instances WHERE id="..id..";")
+	if(result ~= nil)then
+		local colcount = result:GetColumnCount();
+		repeat
+			for col = 0, colcount-1, 1 do
+				string_data[col] = result:GetColumn( col ):GetString()
+				local b1 = string.find(string_data[col], "36612")
+				local b2 = string.find(string_data[col], "36855")
+				local b3 = string.find(string_data[col], "37813")
+				local b4 = string.find(string_data[col], "36626")
+				local b5 = string.find(string_data[col], "36627")
+				local b6 = string.find(string_data[col], "36678")
+				local b7 = string.find(string_data[col], "37970")
+				local b8 = string.find(string_data[col], "37955")
+				local b9 = string.find(string_data[col], "36789")
+				local b10 = string.find(string_data[col], "36853")
+				local b11 = string.find(string_data[col], "36597")
+				if(b1 ~= nil)then
+					INSTANCE_ICC[id].marrowgar = true
+				end
+				if(b2 ~= nil)then
+					INSTANCE_ICC[id].deathwhisper = true
+				end
+				if(b3 ~= nil)then
+					INSTANCE_ICC[id].saurfang = true
+				end
+				if(b4 ~= nil)then
+					INSTANCE_ICC[id].festergut = true
+				end
+				if(b5 ~= nil)then
+					INSTANCE_ICC[id].rotface = true
+				end
+				if(b6 ~= nil)then
+					INSTANCE_ICC[id].prof = true
+				end
+				if(b7 ~= nil)then
+					INSTANCE_ICC[id].prince = true
+				end
+				if(b8 ~= nil)then
+					INSTANCE_ICC[id].bloodqueen = true
+				end
+				if(b9 ~= nil)then
+					INSTANCE_ICC[id].dreamwalker = true
+				end
+				if(b10 ~= nil)then
+					INSTANCE_ICC[id].sindragosa = true
+				end
+				if(b11 ~= nil)then
+					INSTANCE_ICC[id].lichking = true
+				end
+			end
+		until result:NextRow() ~= true;
+	end
+end
+if(INSTANCE_ICC[id].lichking ~= true and pPlayer:HasAura(SPELL_CHILL) == false)then
+	SetDBCSpellVar(SPELL_CHILL, "c_is_flags", 0x01000)
+	pPlayer:CastSpell(SPELL_CHILL)
+end
+end
+
+function OnGOpush(pGO)
+local plr = pGO:GetClosestPlayer()
+if(plr)then
+	local id = plr:GetInstanceID()
+	for b = 1,#object_link do
+		if(pGO:GetEntry() == object_link[b][2] and object_link[b][1] == 36612 and INSTANCE_ICC[id].marrowgar)then
+			pGO:Activate()
+		end
+		if(pGO:GetEntry() == object_link[b][2] and object_link[b][1] == 36855 and INSTANCE_ICC[id].deathwhisper)then
+			pGO:Activate()
+		end
+		if(pGO:GetEntry() == object_link[b][2] and object_link[b][1] == 37813 and INSTANCE_ICC[id].saurfang)then
+			pGO:Activate()
+		end
+		if(pGO:GetEntry() == object_link[b][2] and object_link[b][1] == 36626 and INSTANCE_ICC[id].festergut and INSTANCE_ICC[id].rotface == nil)then
+			pGO:Activate()
+		elseif(pGO:GetEntry() == object_link[b][2] and object_link[b][1] == 36626 and INSTANCE_ICC[id].festergut and INSTANCE_ICC[id].rotface)then
+			pGO:Despawn(1,0)
+		end
+		if(pGO:GetEntry() == object_link[b][2] and object_link[b][1] == 36627 and INSTANCE_ICC[id].rotface and INSTANCE_ICC[id].festergut == nil)then
+			pGO:Activate()
+		elseif(pGO:GetEntry() == object_link[b][2] and object_link[b][1] == 36627 and INSTANCE_ICC[id].rotface and INSTANCE_ICC[id].festergut)then
+			pGO:Despawn(1,0)
+		end
+		if(pGO:GetEntry() == object_link[b][2] and object_link[b][1] == -1 and INSTANCE_ICC[id].rotface and INSTANCE_ICC[id].festergut)then
+			pGO:Activate()
+		end
+		if(pGO:GetEntry() == object_link[b][2] and object_link[b][1] == 36678 and INSTANCE_ICC[id].prof)then
+			pGO:Activate()
+		end
+		if(pGO:GetEntry() == object_link[b][2] and object_link[b][1] == 37970 and INSTANCE_ICC[id].prince)then
+			pGO:Activate()
+		end
+		if(pGO:GetEntry() == object_link[b][2] and object_link[b][1] == 37955 and INSTANCE_ICC[id].bloodqueen)then
+			pGO:Activate()
+		end
+		if(pGO:GetEntry() == object_link[b][2] and object_link[b][1] == 36789 and INSTANCE_ICC[id].dreamwalker)then
+			pGO:Activate()
+		end
+		if(pGO:GetEntry() == object_link[b][2] and object_link[b][1] == 36853 and INSTANCE_ICC[id].sindragosa)then
+			pGO:Activate()
+		end
+		if(pGO:GetEntry() == object_link[b][2] and object_link[b][1] == -2 and INSTANCE_ICC[id].sindragosa and INSTANCE_ICC[id].prof and INSTANCE_ICC[id].bloodqueen)then
+			pGO:Activate()
+		end
+	end
+end
+end
+
+function TeleportOnGossip(pGO, event, pPlayer)
+local id = pPlayer:GetInstanceID()
+pGO:GossipCreateMenu(100, pPlayer, 0)
+pGO:GossipMenuAddItem(0,"Light's Hammer", 1, 0)
+if(INSTANCE_ICC[id].marrowgar)then
+	pGO:GossipMenuAddItem(0,"Oratory of the Damned", 2, 0)
+end
+if(INSTANCE_ICC[id].deathwhisper)then
+	pGO:GossipMenuAddItem(0,"Rampart of Skulls", 3, 0)
+	pGO:GossipMenuAddItem(0,"Deathbringer's Rise", 4, 0) -- gunship battle needs core support.
+end
+if(INSTANCE_ICC[id].saurfang)then
+	pGO:GossipMenuAddItem(0,"The Upper Spire", 5, 0)
+end
+if(INSTANCE_ICC[id].sindragosa)then
+	pGO:GossipMenuAddItem(0,"The Frost Queen's Lair", 6, 0)
+end
+pGO:GossipSendMenu(pPlayer)
+end
+
+function TeleporterOnSelect(pGO, event, pPlayer, id, intid, code)
+for i = 1, #tele_coords do
+	if(intid == tele_coords[i][1])then
+		pPlayer:CastSpell(tele_coords[i][2])
+	end
+end
+pPlayer:GossipComplete()
+end
+ --[[
+function DummySpellTeleport(spellIndex, pSpell)
+local caster = pSpell:GetCaster() -- TODO: Script the teleport spells.
+caster:Teleport(631,)
+end
+]]--
+RegisterInstanceEvent(631,2,DoorOnLoad)
+for i = 1, #object_link do
+RegisterGameObjectEvent(object_link[i][2],2,OnGOpush)
+end
+for i = 1, #scourge_ports do
+RegisterGameObjectEvent(scourge_ports[i][1],4,TeleportOnGossip)
+end
+for i = 1, #scourge_ports do
+RegisterGOGossipEvent(scourge_ports[i][1],2,TeleporterOnSelect)
+end
