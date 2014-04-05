4591964c42c058ba874034bff87dbe4201d11ed0
Remko de Bruin <altogoten@gmail.com>
2014-04-05 13:25:15 +0200

    Legendary moved & edit

--- Fun/Legendary.lua
+++ Fun/Legendary.lua
@@ -1,10 +1,12 @@
  --[[Script by DarkAngel39]]--
+   --[[Tested by Goten87]]--
 
 function LegendaryLoot(event, pPlayer, pTarget, Money, ItemId)
 	print("looted")
 	local itemquality = WorldDBQuery("SELECT quality FROM items WHERE entry="..ItemId..";"):GetColumn(0):GetString()
 	if(itemquality == "5")then
 		local itemname = WorldDBQuery("SELECT name1 FROM items WHERE entry="..ItemId..";"):GetColumn(0):GetString()
+		SendWorldMessage(pPlayer:GetName().." Looted "..itemname..".",1)
 		SendWorldMessage(pPlayer:GetName().." Looted "..itemname..".",2)
 	end
 end
